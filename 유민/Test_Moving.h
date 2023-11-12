d80f06a7c7cb1a2b64c691361f514d810d9e060c
tracer12 <vendetta12@naver.com>
2023-11-12 23:12:08 +0900

    Update Test_Moving.h

--- 유민/Test_Moving.h
+++ 유민/Test_Moving.h
@@ -8,14 +8,14 @@ extern int npcCurPosX, npcCurPosY;
 extern clock_t NPC_current_Time;
 
 
-void ShowNpcBlock();
-void DeleteBlock();
-int NPC_moveLeft();
-int NPC_moveRight();
-int NPC_moveUp();
-int NPC_moveDown();
-int NpcMoving(clock_t current_game_time);
-void SetNpcPos();
+void ShowNpcBlock(); // NPC 아이콘 출력 함수
+void DeleteBlock(); // NPC 아이콘 삭제함수
+int NPC_moveLeft(); // NPC 좌측 이동
+int NPC_moveRight(); // NPC 우측 이동
+int NPC_moveUp(); // NPC 상단 이동
+int NPC_moveDown(); // NPC 하단 이동
+int NpcMoving(clock_t current_game_time); // NPC 이동함수, 여기서 shortestDistance 함수 호출해서 one_srt_dist 배열을 만든다
+void SetNpcPos(); // 첫 NPC 위치 
 
 
 #endif
