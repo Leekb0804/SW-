5eca0d50b2a83089314a47d911022f853b671b0e
tracer12 <vendetta12@naver.com>
2023-11-12 23:14:42 +0900

    Update calculatorDST.h

--- 유민/calculatorDST.h
+++ 유민/calculatorDST.h
@@ -15,8 +15,8 @@ typedef struct queue {	//BFS 탐색에 쓰일 큐
 	int dist;
 }Queue;
 
-void bfs(Queue* q, int x, int y); // BFS탐색, 목적지까지의 최단거리를 구해줌
-void dfs(int x, int y, int npcX, int npcY); // DFS로 one_srt_dist 배열 완성
-int ShortestDistance(int npcX, int npcY);
+void bfs(Queue* q, int x, int y); // BFS탐색, 조건을 고려하면서 목적지를 구하고 목적지까지의 최단거리를 구해줌
+void dfs(int x, int y, int npcX, int npcY); // 신경 ㄴㄴ
+int ShortestDistance(int npcX, int npcY); // BFS,DFS를 이용해서 one_srt_dist를 만들어줌
 
 #endif
